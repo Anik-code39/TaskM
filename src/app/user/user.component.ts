98b4e3a7aaa2803c5e79f5b817b794eb8560f346
Anik Saha <anik@erainfotechbd.com>
2025-03-11 16:13:24 +0600

    last one

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,6 +15,7 @@ export class UserComponent {
   @Input( {required : true }) avatar!: string;
   @Input( {required : true }) name!: string;
   @Output( ) select = new EventEmitter();
+  // select= output<string>();
 
   get ImagePath() {
     return 'assets/users/'+ this.avatar;
